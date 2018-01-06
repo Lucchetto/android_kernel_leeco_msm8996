5bcb9f85534c77654b10a26f0776948c666078b6
celtare21 <celtare21@gmail.com>
2018-01-06 22:40:35 +0200

    Actually fix build

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -335,6 +335,23 @@ EXPORT_SYMBOL(kthread_create_on_node);
  * except that @cpu doesn't need to be online, and the thread must be
  * stopped (i.e., just returned from kthread_create()).
  */
+static void __kthread_bind_mask(struct task_struct *p, const struct cpumask *mask, long state)
+{
+	unsigned long flags;
+
+	if (!wait_task_inactive(p, state)) {
+		WARN_ON(1);
+		return;
+	}
+
+	/* It's safe because the task is inactive. */
+	raw_spin_lock_irqsave(&p->pi_lock, flags);
+	do_set_cpus_allowed(p, mask);
+	p->flags |= PF_NO_SETAFFINITY;
+	raw_spin_unlock_irqrestore(&p->pi_lock, flags);
+}
+
+
 static void __kthread_bind(struct task_struct *p, unsigned int cpu, long state)
 {
 	__kthread_bind_mask(p, cpumask_of(cpu), state);
