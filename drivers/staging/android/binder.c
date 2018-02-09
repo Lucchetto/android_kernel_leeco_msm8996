3f219afe97222edf237f22deaf266f41eb989ea9
celtare21 <celtare21@gmail.com>
2018-02-09 01:10:32 +0000

    Revert "android: binder: fix proc->files use-after-free"
    This reverts commit a346dda204dba488a74cec52e2736c4940995009.

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -462,33 +462,9 @@ struct binder_ref {
 };
 
 enum binder_deferred_state {
-	BINDER_DEFERRED_FLUSH        = 0x01,
-	BINDER_DEFERRED_RELEASE      = 0x02,
-	BINDER_ZOMBIE_CLEANUP        = 0x04,
-};
-
-struct binder_seq_head {
-	int active_count;
-	int max_active_count;
-	spinlock_t lock;
-	struct list_head active_threads;
-	u64 lowest_seq;
-};
-
-#define SEQ_BUCKETS 16
-struct binder_seq_head binder_active_threads[SEQ_BUCKETS];
-struct binder_seq_head zombie_procs;
-
-static inline int binder_seq_hash(struct binder_thread *thread)
-{
-	u64 tp = (u64)thread;
-
-	return ((tp>>8) ^ (tp>>12)) % SEQ_BUCKETS;
-}
-
-struct binder_seq_node {
-	struct list_head list_node;
-	u64 active_seq;
+	BINDER_DEFERRED_PUT_FILES    = 0x01,
+	BINDER_DEFERRED_FLUSH        = 0x02,
+	BINDER_DEFERRED_RELEASE      = 0x04,
 };
 
 /**
@@ -573,7 +549,7 @@ struct binder_proc {
 	struct list_head waiting_threads;
 	int pid;
 	struct task_struct *tsk;
-	struct files_struct *zombie_files;
+	struct files_struct *files;
 	struct hlist_node deferred_work_node;
 	int deferred_work;
 	bool is_dead;
@@ -922,33 +898,26 @@ static void binder_free_thread(struct binder_thread *thread);
 static void binder_free_proc(struct binder_proc *proc);
 static void binder_inc_node_tmpref_ilocked(struct binder_node *node);
 
-struct files_struct *binder_get_files_struct(struct binder_proc *proc)
-{
-	return get_files_struct(proc->tsk);
-}
-
 static int task_get_unused_fd_flags(struct binder_proc *proc, int flags)
 {
-	struct files_struct *files;
+	struct files_struct *files = proc->files;
 	unsigned long rlim_cur;
 	unsigned long irqs;
 	int ret;
 
-	files = binder_get_files_struct(proc);
 	if (files == NULL)
 		return -ESRCH;
 
-	if (!lock_task_sighand(proc->tsk, &irqs)) {
-		ret = -EMFILE;
-		goto err;
-	}
+	if (!lock_task_sighand(proc->tsk, &irqs))
+		return -EMFILE;
 
 	rlim_cur = task_rlimit(proc->tsk, RLIMIT_NOFILE);
 	unlock_task_sighand(proc->tsk, &irqs);
 
+	preempt_enable_no_resched();
 	ret = __alloc_fd(files, 0, rlim_cur, flags);
-err:
-	put_files_struct(files);
+	preempt_disable();
+
 	return ret;
 }
 
@@ -958,11 +927,10 @@ err:
 static void task_fd_install(
 	struct binder_proc *proc, unsigned int fd, struct file *file)
 {
-	struct files_struct *files = binder_get_files_struct(proc);
-
-	if (files) {
-		__fd_install(files, fd, file);
-		put_files_struct(files);
+	if (proc->files) {
+		preempt_enable_no_resched();
+		__fd_install(proc->files, fd, file);
+		preempt_disable();
 	}
 }
 
@@ -971,20 +939,18 @@ static void task_fd_install(
  */
 static long task_close_fd(struct binder_proc *proc, unsigned int fd)
 {
-	struct files_struct *files = binder_get_files_struct(proc);
 	int retval;
 
-	if (files == NULL)
+	if (proc->files == NULL)
 		return -ESRCH;
 
-	retval = __close_fd(files, fd);
+	retval = __close_fd(proc->files, fd);
 	/* can't restart close syscall because file table entry was cleared */
 	if (unlikely(retval == -ERESTARTSYS ||
 		     retval == -ERESTARTNOINTR ||
 		     retval == -ERESTARTNOHAND ||
 		     retval == -ERESTART_RESTARTBLOCK))
 		retval = -EINTR;
-	put_files_struct(files);
 
 	return retval;
 }
@@ -4827,6 +4793,7 @@ static void binder_vma_close(struct vm_area_struct *vma)
 		     (vma->vm_end - vma->vm_start) / SZ_1K, vma->vm_flags,
 		     (unsigned long)pgprot_val(vma->vm_page_prot));
 	binder_alloc_vma_close(&proc->alloc);
+	binder_defer_work(proc, BINDER_DEFERRED_PUT_FILES);
 }
 
 static int binder_vm_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
@@ -4868,8 +4835,10 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 	vma->vm_private_data = proc;
 
 	ret = binder_alloc_mmap_handler(&proc->alloc, vma);
-
-	return ret;
+	if (ret)
+		return ret;
+	proc->files = get_files_struct(current);
+	return 0;
 
 err_bad_arg:
 	pr_err("binder_mmap: %d %lx-%lx %s failed %d\n",
@@ -5048,9 +5017,7 @@ static void binder_deferred_release(struct binder_proc *proc)
 	struct rb_node *n;
 	int threads, nodes, incoming_refs, outgoing_refs, active_transactions;
 
-	binder_proc_lock(proc, __LINE__);
-	binder_queue_for_zombie_cleanup(proc);
-	binder_proc_unlock(proc, __LINE__);
+	BUG_ON(proc->files);
 
 	rt_mutex_lock(&binder_procs_lock);
 	hlist_del_init(&proc->proc_node);
@@ -5145,6 +5112,13 @@ static void binder_deferred_func(struct work_struct *work)
 			defer = 0;
 		}
 		rt_mutex_unlock(&binder_deferred_lock);
+
+		files = NULL;
+		if (defer & BINDER_DEFERRED_PUT_FILES) {
+			files = proc->files;
+			if (files)
+				proc->files = NULL;
+		}
 
 		if (defer & BINDER_DEFERRED_FLUSH)
 			binder_deferred_flush(proc);
